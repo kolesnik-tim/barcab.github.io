2f8bc90b11e06d9be00c55b8c80d84118554c2a1
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-05-18 13:22:04 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -4,12 +4,28 @@ import './import/input.js';
 import './import/rangeslider.js';
 
 
-// //menu-open
-// $('.menu-open').on('click', function() {
-//   $('.menu__mobile').fadeIn();
-//   $('.menu--bg').fadeIn();
-// });
-// $('.menu--bg, .close, .menu__mobile a').on('click', function() {
-//   $('.menu__mobile').fadeOut();
-//   $('.menu--bg').fadeOut();
-// });
+//menu-open
+$('.menu-open, .pop-up ul a').on('click', function() {
+  $('.pop-up').fadeToggle();
+  $('.header__logo').fadeToggle();
+  $('.menu-open').toggleClass('active');
+  $('body').toggleClass('modal');
+});
+
+//header__logo
+$(window).scroll(function() {
+  if($(this).scrollTop() > 250) {
+    $('.header').addClass('active');
+    $('.header__logo').fadeIn();
+  } else{
+    $('.header').removeClass('active');
+    $('.header__logo').fadeOut();
+  }
+});
+
+$('#menu1, #menu').on('click','a', function(event) {
+  event.preventDefault();
+  var id  = $(this).attr('href'),
+    top = $(id).offset().top;
+  $('body,html').animate({scrollTop: top}, 1500);
+});
