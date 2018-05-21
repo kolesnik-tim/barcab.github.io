dfd95e973b58be8cb401a707368eafa815d8f826
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-05-21 17:45:26 +0300

    fix

--- src/js/import/scrollmagic.js
+++ src/js/import/scrollmagic.js
@@ -3,6 +3,9 @@ import TweenMax from 'gsap';
 import 'animation.gsap';
 import 'debug.addIndicators';
 
+
+
+//step
 var controller = new ScrollMagic.Controller();
 var tween = TweenMax.to('#animate, #animate-text', 0.9, {opacity: 1, left: 0, top: 0});
 var scene = new ScrollMagic.Scene({triggerElement: '#trigger', duration: '20%'})
@@ -34,12 +37,25 @@ var scene = new ScrollMagic.Scene({triggerElement: '#trigger5', duration: '20%'}
 
 
 
-
+//number
 var controller = new ScrollMagic.Controller();
-var scene = new ScrollMagic.Scene({triggerElement: '#trigger22', duration: 1300})
+var scene = new ScrollMagic.Scene({triggerElement: '#trigger22', duration: 1500})
   .addTo(controller)
   .on('progress', function(e) {
     $('#progress').text((e.progress * 10000).toFixed(0));
   });
 
+
+var tween = TweenMax.to('#animate6', 0.9, {opacity: 1, left: 0});
+var scene = new ScrollMagic.Scene({triggerElement: '#trigger6', duration: '20%'})
+  .setTween(tween)
+  .addTo(controller);
+var tween = TweenMax.to('#animate7', 0.9, {opacity: 1, right: 0});
+var scene = new ScrollMagic.Scene({triggerElement: '#trigger7', duration: '20%'})
+  .setTween(tween)
+  .addTo(controller);
   
+
+
+
+
